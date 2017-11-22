e92cf1ee0f74de233586babef72b931cc29791f7
Vyacheslav Mikhailov <slava.lion@gmail.com>
2017-11-22 23:02:00 +0400

    first commit

---
+++ lib/elixir_serialport.ex
@@ -0,0 +1,18 @@
+defmodule ElixirSerialport do
+  @moduledoc """
+  Documentation for ElixirSerialport.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirSerialport.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_serialport_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirSerialportTest do
+  use ExUnit.Case
+  doctest ElixirSerialport
+
+  test "greets the world" do
+    assert ElixirSerialport.hello() == :world
+  end
+end
